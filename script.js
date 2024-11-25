8c56a22f2fd19d79fbb32a2cf98f086bcda6af6e
shristhi-29 <shristhisingh2908@gmail.com>
2024-11-25 16:10:34 +0530

    digital_clock using_js

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 
 
-function clr(){
-    document.getElementById("result").value="";
+ function showTime(){
+var date= new Date();
+var h=date.getHours();
+var m=date.getMinutes();
+var s=date.getSeconds();
+var session= "A.M."
 
+
+if(h==0){
+   h=12;
 }
 
-function display(val){
-document.getElementById("result").value+=val;
+
+if(h>12){
+    h=h-12;
+    session="P.M."
 }
-function equate(){
-    let x=document.getElementById("result").value;
-    let y=eval(x);
-    document.getElementById("result").value=y;
-}
+
+h=(h<10)?"0"+h:h;
+m=(m<10)?"0"+m:m;
+s=(s<10)?"0"+s:s;
+
+
+ document.getElementById("DigitalClock").innerHTML=h + ":" + m + ":"+ s + " " + session;
+ 
+  setTimeout(showTime , 1000)
+ } 
+showTime()
+
+
+
